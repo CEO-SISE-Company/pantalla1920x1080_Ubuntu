af880562174635cd6502cdce27d36b667d35a466
CEO-SISE-Company <73506610+CEO-SISE-Company@users.noreply.github.com>
2020-12-06 20:53:03 -0500

    Create starter.py

---
+++ starter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#Let's import the OS library
+import os
+
+#Let's run the resizer script
+os.system("echo <type your password here> | sudo -S <type the path of your resizer script here>")
